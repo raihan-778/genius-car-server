324aba53702710f119c7abb26d1c62fdcc6311b3
raihan-778 <raihanarif778@gmail.com>
2022-11-03 17:24:48 +0600

    single find function added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 
@@ -13,26 +13,41 @@ app.use(cors());
 //this is auto convertion to json file when post method is applied.
 app.use(express.json());
 
-console.log(process.env.DB_USER);
-console.log(process.env.SECREAT_KEY);
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.SECREAT_KEY}@cluster0.jz1qjld.mongodb.net/?retryWrites=true&w=majority`;
 
-console.log(uri);
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-client.connect((err) => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
+//mongodb crud operation
+
+async function run() {
+  try {
+    const serviceCollection = client.db("geniusCar").collection("services");
+    app.get("/services", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await serviceCollection.findOne(query);
+      res.send(service);
+    });
+  } finally {
+  }
+}
+run().catch((err) => console.error(err));
 
 app.get("/", (req, res) => {
   res.send("genius car server is running");
 });
+
 app.listen(port, () => {
   console.log(`genius car server is running on port ${port}`);
 });
