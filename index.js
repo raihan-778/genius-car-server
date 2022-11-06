b9b7f59eb64b1c33f9192eba59c04a90445fb51b
raihan-778 <raihanarif778@gmail.com>
2022-11-06 18:17:09 +0600

    ps

--- index.js
+++ index.js
@@ -1,94 +1,100 @@
-const express = require("express")
-const cors = require("cors")
-const { MongoClient, ServerApiVersion, ObjectId, ObjectID } = require("mongodb")
-require("dotenv").config()
-const app = express()
+const express = require("express");
+const cors = require("cors");
+const {
+  MongoClient,
+  ServerApiVersion,
+  ObjectId,
+  ObjectID,
+} = require("mongodb");
+require("dotenv").config();
+const app = express();
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
 //middlewares
+/*  p7QVdeu9tlVOsfgO*/
 // this is for cross platform permissions
-app.use(cors())
+app.use(cors());
 
 //this is auto convertion to json file when post method is applied.
-app.use(express.json())
+app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.SECREAT_KEY}@cluster0.jz1qjld.mongodb.net/?retryWrites=true&w=majority`
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.SECREAT_KEY}@cluster0.jz1qjld.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
-})
+});
 
 //mongodb crud operation
 
 async function run() {
   try {
-    const serviceCollection = client.db("geniusCar").collection("services")
-    const orderCollection = client.db("geniusCar").collection("orders")
+    const serviceCollection = client.db("geniusCar").collection("services");
+    const orderCollection = client.db("geniusCar").collection("orders");
     app.get("/services", async (req, res) => {
-      const query = {}
-      const cursor = serviceCollection.find(query)
-      const services = await cursor.toArray()
-      res.send(services)
-    })
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
 
     app.get("/services/:id", async (req, res) => {
-      const id = req.params.id
-      const query = { _id: ObjectId(id) }
-      const service = await serviceCollection.findOne(query)
-      res.send(service)
-    })
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await serviceCollection.findOne(query);
+      res.send(service);
+    });
 
     //orders api
     app.get("/orders", async (req, res) => {
-      console.log(req.query.email)
-      let query = {}
+      console.log(req.query.email);
+      let query = {};
       if (req.query.email) {
         query = {
           email: req.query.email,
-        }
+        };
       }
-      const cursor = orderCollection.find(query)
-      const orders = await cursor.toArray()
-      res.send(orders)
-    })
+      const cursor = orderCollection.find(query);
+      const orders = await cursor.toArray();
+      res.send(orders);
+    });
 
     app.post("/orders", async (req, res) => {
-      const order = req.body
-      const result = await orderCollection.insertOne(order)
-      res.send(result)
-    })
+      const order = req.body;
+      const result = await orderCollection.insertOne(order);
+      res.send(result);
+    });
 
     app.patch("/orders/:id", async (req, res) => {
-      const id = req.params.id
-      const status = req.body.status
-      const query = { _id: ObjectId(id) }
+      const id = req.params.id;
+      const status = req.body.status;
+      const query = { _id: ObjectId(id) };
       const updatedDoc = {
         $set: {
           status: status,
         },
-      }
-      const result = await orderCollection.updateOne(query, updatedDoc)
-      res.send(result)
-    })
+      };
+      const result = await orderCollection.updateOne(query, updatedDoc);
+      res.send(result);
+    });
 
     app.delete("/orders/:id", async (req, res) => {
-      const id = req.params.id
-      const query = { _id: ObjectId(id) }
-      const result = await orderCollection.deleteOne(query)
-      res.send(result)
-    })
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await orderCollection.deleteOne(query);
+      res.send(result);
+    });
   } finally {
   }
 }
-run().catch((err) => console.error(err))
+run().catch((err) => console.error(err));
 
 app.get("/", (req, res) => {
-  res.send("genius car server is running")
-})
+  res.send("genius car server is running");
+});
 
 app.listen(port, () => {
-  console.log(`genius car server is running on port ${port}`)
-})
+  console.log(`genius car server is running on port ${port}`);
+});
